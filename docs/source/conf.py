a509ce419f55fc6ff3bf90fd81b384ef93a75f41
Lillian Helms <141255627+lhelms2@users.noreply.github.com>
2023-08-16 14:45:21 -0500

    Update conf.py commenting out logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,11 +31,11 @@ html_static_path = ['_static']
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
-html_logo = "images/logo_test1.png"
-html_theme_options = {
-    'logo_only': True,
-    'display_version': False,
-}
+# -- html_logo = "images/logo_test1.png"
+# -- html_theme_options = {
+# --    'logo_only': True,
+# --    'display_version': False,
+# -- }
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
