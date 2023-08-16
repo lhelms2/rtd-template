4445c44598fdc1bf094bb40226f6df0dbed68f85
Lillian Helms <141255627+lhelms2@users.noreply.github.com>
2023-08-16 14:09:59 -0500

    Update conf.py logo test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,6 +31,7 @@ html_static_path = ['_static']
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
+html_logo = "images/logo_test.png"
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
